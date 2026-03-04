c8d70b180f1c594d0998f2d88ba55921674826b3
Shane <chapmanshane23@gmail.com>
2026-03-04 14:11:17 +1300

    changed random button. added footer with email address.

--- src/components/CharacterSelector.tsx
+++ src/components/CharacterSelector.tsx
@@ -29,22 +29,12 @@ export default function CharacterSelector() {
   return (
     <div className="flex flex-col py-2 p-4 bg-surface rounded-xl shadow-card/40 border-3 max-w-90 max-h-[70vh] items-center font-inter">
       <div className="flex gap-2 items-center w-full mb-2">
-        {/* Dice */}
         <button
-          className="p-[4px] flex flex-col h-10 w-10 rounded-lg bg-accent text-text font-inter font-extrabold mx-left  m-2 btn justify-between"
+          className="p-[4px] flex flex-col w-full rounded-lg bg-accent text-text font-inter font-extrabold mx-left  m-2 btn justify-between"
           onClick={randomizeConfig}
         >
-          <div className="flex justify-between">
-            <div className="h-[8px] w-[8px] bg-black rounded-[3px]"></div>
-            <div className="h-[8px] w-[8px] bg-black rounded-[3px]"></div>
-          </div>
-          <div className="h-[8px] w-[8px] bg-black rounded-[3px] mx-auto"></div>
-          <div className="flex justify-between">
-            <div className="h-[8px] w-[8px] bg-black rounded-[3px]"></div>
-            <div className="h-[8px] w-[8px] bg-black rounded-[3px]"></div>
-          </div>
+          RANDOM
         </button>
-        <h1 className="text-center text-accent text-3xl">Randomize</h1>
       </div>
       <div className="w-full">
         {/* Tab bar */}
